726e4242db2684e2e01de82c94c9c29232e23630
ayase-mstk <masa1215.hayase@gmail.com>
2024-07-14 01:55:36 +0900

    :bug: register parent node

--- vendor/piscine/btreeinsertdata.go
+++ vendor/piscine/btreeinsertdata.go
@@ -1,10 +1,5 @@
 package piscine
 
-type TreeNode struct {
-	Left, Right, Parent *TreeNode
-	Data                string
-}
-
 func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 	if root == nil {
 		return &TreeNode{Data: data}
@@ -12,8 +7,10 @@ func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 
 	if data < root.Data {
 		root.Left = BTreeInsertData(root.Left, data)
+		root.Left.Parent = root
 	} else {
 		root.Right = BTreeInsertData(root.Right, data)
+		root.Right.Parent = root
 	}
 
 	return root
