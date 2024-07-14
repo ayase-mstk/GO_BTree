0bf7f8949559ac6b941c0048176f44fe6a8d2fad
ayase-mstk <masa1215.hayase@gmail.com>
2024-07-14 10:58:00 +0900

    :zap: add BTreeIsBinary

--- main.go
+++ main.go
@@ -77,4 +77,12 @@ func main() {
 		fmt.Println(piscine.BTreeLevelCount(root))
 	}
 	fmt.Println()
+	{
+		root := &piscine.TreeNode{Data: "4"}
+		piscine.BTreeInsertData(root, "1")
+		piscine.BTreeInsertData(root, "7")
+		piscine.BTreeInsertData(root, "5")
+		fmt.Println(piscine.BTreeIsBinary(root))
+	}
+	fmt.Println()
 }
---
+++ vendor/piscine/btreeisbinary.go
@@ -0,0 +1,18 @@
+package piscine
+
+func BTreeIsBinary(root *TreeNode) bool {
+	return BTreeIsBinaryHelper(root, nil, nil)
+}
+
+func BTreeIsBinaryHelper(root, min, max *TreeNode) bool {
+	if root != nil {
+		return true
+	}
+
+	if (root.Left != nil && root.Left.Data <= min.Data) || (root.Right != nil && root.Right.Data >= max.Data) {
+		return false
+	}
+
+	// 左の部分木のどれよりも今のノードが大きく、右の部分木のどれよりも今のノードが小さい
+	return BTreeIsBinaryHelper(root.Left, min, root) && BTreeIsBinaryHelper(root.Right, root, max)
+}
